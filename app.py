e743087bb75ecdb1ec47831ddff3528000498e1e
arun <arunkumar39874@gmail.com>
2024-01-29 14:22:17 +0530

    Updated

--- app.py
+++ app.py
@@ -4,16 +4,12 @@ import joblib
 import librosa
 import numpy as np
 
-# Load the trained model
 model = joblib.load("voice_model.pkl")
 
-# Streamlit app
 st.title("Voice Gender Detection")
 
-# File upload
 uploaded_file = st.file_uploader("Upload an audio file", type=["wav"])
 
-# Function to extract audio features
 def extract_features(audio_data, _):
     try:
         audio_data = audio_data.astype(np.float32)
@@ -23,7 +19,6 @@ def extract_features(audio_data, _):
         print("Error extracting features:", str(e))
         return None
 
-# Function to predict gender
 def predict_gender(model, audio_data):
     features = extract_features(audio_data, 44100)
 
@@ -34,18 +29,14 @@ def predict_gender(model, audio_data):
     else:
         return None
 
-# Perform prediction when the button is pressed
 if uploaded_file is not None:
-    # Use librosa to load the audio content directly
     audio_data, _ = librosa.load(uploaded_file, sr=None)
 
-    # Perform prediction
     predicted_gender = predict_gender(model, audio_data)
     if predicted_gender==1:
         predicted_gender = "Male"
     else:
         predicted_gender = "Female"
-    # Display the result
     if predicted_gender is not None:
         st.success(f"Predicted Gender: {predicted_gender}")
     else:
--- training.py
+++ training.py
@@ -5,30 +5,23 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import joblib
 
-# Load the dataset from Excel CSV file
 file_path = "voice.csv"
 df = pd.read_csv(file_path)
 
-# Separate features (X) and labels (y)
 X = df.drop("gender", axis=1)
 y = df["gender"]
 
-# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Standardize features (important for SVM)
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Train the SVM model
 model = SVC(kernel='linear', C=1)
 model.fit(X_train_scaled, y_train)
 
-# Save the trained model
 joblib.dump(model, "voice_model.pkl")
 
-# Evaluate the model on the test set
 y_pred = model.predict(X_test_scaled)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy on the test set: {accuracy * 100:.2f}%")
